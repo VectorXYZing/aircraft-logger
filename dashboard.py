039a05423fdf863553bf12f51e9ef9d6e5594267
VectorXYZing <paul@steele.com.au>
2026-01-08 18:59:16 +1100

    Dashboard: v1.2.1 - Expanded airline prefix database and optimized CPU performance

--- dashboard.py
+++ dashboard.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 import time
 
 app = Flask(__name__)
-VERSION = "1.2.0"
+VERSION = "1.2.1"
 
 # Logging setup
 LOGGING_DIR = os.path.expanduser('~/aircraft-logger/logs')
