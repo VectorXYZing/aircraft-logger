ff1c24038d1274bcd43c067e052818e4465bb592
VectorXYZing <paul@steele.com.au>
2026-01-08 19:05:08 +1100

    Dashboard: v1.2.2 - Aggressive operator detection using API callsigns and prefixes

--- dashboard.py
+++ dashboard.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 import time
 
 app = Flask(__name__)
-VERSION = "1.2.1"
+VERSION = "1.2.2"
 
 # Logging setup
 LOGGING_DIR = os.path.expanduser('~/aircraft-logger/logs')
